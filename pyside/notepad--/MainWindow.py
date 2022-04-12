f18b41106180761847a6184d52289103e0a96b07
Maksymilian Jopek <maks@jopek.eu>
2022-04-12 21:41:18 +0200

    Notepad-- v1.0.0

--- pyside/notepad--/MainWindow.py
+++ pyside/notepad--/MainWindow.py
@@ -3,21 +3,33 @@
 ################################################################################
 ## Form generated from reading UI file 'Main.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.3
+## Created by: Qt User Interface Compiler version 6.2.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QTextEdit, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1000, 1000)
+        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
         self.actionNew = QAction(MainWindow)
         self.actionNew.setObjectName(u"actionNew")
         self.actionOpen = QAction(MainWindow)
@@ -44,15 +56,27 @@ class Ui_MainWindow(object):
         self.actionFont.setObjectName(u"actionFont")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(-7, -1, 811, 621))
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.textEdit = QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName(u"textEdit")
+
+        self.verticalLayout.addWidget(self.textEdit)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 19))
+        self.menubar.setGeometry(QRect(0, 0, 1000, 19))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
+        self.menuFile.setGeometry(QRect(4419, 154, 123, 110))
         self.menuDecorate = QMenu(self.menubar)
         self.menuDecorate.setObjectName(u"menuDecorate")
         self.menuAlign = QMenu(self.menuDecorate)
@@ -88,17 +112,53 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.actionNew.setText(QCoreApplication.translate("MainWindow", u"New", None))
+#if QT_CONFIG(shortcut)
+        self.actionNew.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+N", None))
+#endif // QT_CONFIG(shortcut)
         self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+#if QT_CONFIG(shortcut)
+        self.actionOpen.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+O", None))
+#endif // QT_CONFIG(shortcut)
         self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+#if QT_CONFIG(shortcut)
+        self.actionSave.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+S", None))
+#endif // QT_CONFIG(shortcut)
         self.actionLeft.setText(QCoreApplication.translate("MainWindow", u"Left", None))
+#if QT_CONFIG(shortcut)
+        self.actionLeft.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+L", None))
+#endif // QT_CONFIG(shortcut)
         self.actionMiddle.setText(QCoreApplication.translate("MainWindow", u"Middle", None))
+#if QT_CONFIG(shortcut)
+        self.actionMiddle.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+M", None))
+#endif // QT_CONFIG(shortcut)
         self.actionRight.setText(QCoreApplication.translate("MainWindow", u"Right", None))
+#if QT_CONFIG(shortcut)
+        self.actionRight.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+R", None))
+#endif // QT_CONFIG(shortcut)
         self.actionJustify.setText(QCoreApplication.translate("MainWindow", u"Justify", None))
+#if QT_CONFIG(shortcut)
+        self.actionJustify.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+J", None))
+#endif // QT_CONFIG(shortcut)
         self.actionBold.setText(QCoreApplication.translate("MainWindow", u"Bold", None))
+#if QT_CONFIG(shortcut)
+        self.actionBold.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+B", None))
+#endif // QT_CONFIG(shortcut)
         self.actionUnderline.setText(QCoreApplication.translate("MainWindow", u"Underline", None))
+#if QT_CONFIG(shortcut)
+        self.actionUnderline.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+U", None))
+#endif // QT_CONFIG(shortcut)
         self.actionItalic.setText(QCoreApplication.translate("MainWindow", u"Italic", None))
+#if QT_CONFIG(shortcut)
+        self.actionItalic.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+I", None))
+#endif // QT_CONFIG(shortcut)
         self.actionColor.setText(QCoreApplication.translate("MainWindow", u"Color", None))
+#if QT_CONFIG(shortcut)
+        self.actionColor.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+C", None))
+#endif // QT_CONFIG(shortcut)
         self.actionFont.setText(QCoreApplication.translate("MainWindow", u"Font", None))
+#if QT_CONFIG(shortcut)
+        self.actionFont.setShortcut(QCoreApplication.translate("MainWindow", u"Alt+F", None))
+#endif // QT_CONFIG(shortcut)
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
         self.menuDecorate.setTitle(QCoreApplication.translate("MainWindow", u"Decorate", None))
         self.menuAlign.setTitle(QCoreApplication.translate("MainWindow", u"Align", None))
--- pyside/notepad--/main.py
+++ pyside/notepad--/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel, QLineEdit, QMainWindow, QPlainTextEdit, QPushButton, QSpinBox, QVBoxLayout, QWidget)
+import os
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow, QColorDialog, QFontDialog
 from MainWindow import Ui_MainWindow
 from darkPalette import darkPalette
 
@@ -9,9 +10,67 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
+        self.fname = ""
+        self.actionNew.triggered.connect(self.new) #type: ignore
+        self.actionOpen.triggered.connect(self.open) #type: ignore
+        self.actionSave.triggered.connect(self.save) #type: ignore
+        self.actionBold.triggered.connect(self.set_bold) #type: ignore
+        self.actionItalic.triggered.connect(self.set_italic) #type: ignore
+        self.actionUnderline.triggered.connect(self.set_underline) #type: ignore
+        self.actionColor.triggered.connect(self.set_color) #type: ignore
+        self.actionFont.triggered.connect(self.set_font) #type: ignore
+        self.actionJustify.triggered.connect(self.align_justify) #type: ignore
+        self.actionRight.triggered.connect(self.align_right) #type: ignore
+        self.actionMiddle.triggered.connect(self.align_middle) #type: ignore
+        self.actionLeft.triggered.connect(self.align_left) #type: ignore
+
+    # Menu `Decorate`
+    def align_left(self):
+        self.textEdit.setAlignment(Qt.AlignLeft) #type: ignore
+    def align_middle(self):
+        self.textEdit.setAlignment(Qt.AlignCenter) #type: ignore   
+    def align_right(self):
+        self.textEdit.setAlignment(Qt.AlignRight) #type: ignore
+    def align_justify(self):
+        self.textEdit.setAlignment(Qt.AlignJustify) #type: ignore
+    def set_color(self):
+        self.textEdit.setTextColor(QColorDialog.getColor())
+    def set_font(self):
+        font = QFontDialog.getFont()
+        if font[0] == True:
+            self.textEdit.setFont(font[1])
+    def set_bold(self):
+        self.textEdit.setFontWeight(800 if self.textEdit.fontWeight() == 400 else 400)
+    def set_italic(self):
+        self.textEdit.setFontItalic(not self.textEdit.fontItalic())
+    def set_underline(self):
+        self.textEdit.setFontUnderline(not self.textEdit.fontUnderline())
+
+    # Menu `File`
+    def new(self):
+        self.textEdit.setText("")
+        self.fname = ""
+    def open(self):
+        file_name = QFileDialog.getOpenFileName(self, "Open Text File", os.path.expanduser("~") + "/school/desktopApps/pyside/notepad--", "Text Files (*.txt)") 
+        if file_name[0] == "":
+            return
+        with open(file_name[0], 'r') as f:
+            self.textEdit.setPlainText(f.read())
+        self.fname = file_name[0]
+    def save(self):
+        file_name = QFileDialog.getSaveFileName(self, "Save Text File", os.path.expanduser("~") + "/school/desktopApps/pyside/notepad--/" + self.fname, "Text Files (*.txt)") 
+        if file_name[0] == "":
+            return
+        with open(file_name[0], 'w') as f:
+            f.write(self.textEdit.toPlainText())
+
+
 app = QApplication(sys.argv)
 app.setPalette(darkPalette())
 app.setStyle("Fusion")
+font = app.font()
+font.setPointSize(30)
+app.setFont(font)
 w = MainWindow()
 w.show()
 app.exec()
